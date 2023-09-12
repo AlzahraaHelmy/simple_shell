2d4793c26ca6abae5d6e1cc044fa1901f8700cc2
AlzahraaHelmy <125825678+AlzahraaHelmy@users.noreply.github.com>
2023-09-12 19:58:31 +0300

    Create myshell.h

---
+++ myshell.h
@@ -0,0 +1,18 @@
+#ifndef _MYSHELL_H_
+#define _MYSHELL_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define MAX_INPUT_SIZE 1024
+
+
+#endif
