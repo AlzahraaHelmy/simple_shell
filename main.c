c2651e5d60b039b43e5fc01cc880dba662232698
AlzahraaHelmy <125825678+AlzahraaHelmy@users.noreply.github.com>
2023-09-12 23:40:58 +0300

    Update main.c

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ void remove_newline_at_end(char** buffer, ssize_t* size_read)
 int main(int argc, char* argv[], char* envp[])
 {
     const char delim[2] = " ";
+    int count = 0;
     pid_t pid;
     size_t buff_size;
     ssize_t size_read;
@@ -23,6 +24,7 @@ int main(int argc, char* argv[], char* envp[])
     buffer = (char*)malloc(MAX_INPUT_SIZE * sizeof(char));
     buff_size = (size_t)MAX_INPUT_SIZE;
     signal(SIGINT, handleCtrlC);
+    setbuf(stdout, NULL);
     if (argc > 2)
     {
         printf("%s", envp[0]);
@@ -30,13 +32,14 @@ int main(int argc, char* argv[], char* envp[])
         /* todo imp;ement run from args*/
     }
     while (1) {
+        count++;
         isinteractive = isatty(fileno(stdin));
         if (isinteractive) { /*runnig in interactive mode*/
             printf("#cisfun$ ");
         }
         size_read = getline(&buffer, &buff_size, stdin);
         if (size_read == -1) {
-            exit(0);
+            return(0);
         }
         remove_newline_at_end(&buffer, &size_read);
         tokens = strtok(buffer, delim);
@@ -49,7 +52,7 @@ int main(int argc, char* argv[], char* envp[])
             /*printf("inside if");*/
             continue;
         }
-
+        
         pid = fork();
         if (pid == -1)
         {
@@ -69,9 +72,8 @@ int main(int argc, char* argv[], char* envp[])
         else
         {
             wait(&status);
-
         }
-
+        buffer = NULL;
 
     }
     return (0);
